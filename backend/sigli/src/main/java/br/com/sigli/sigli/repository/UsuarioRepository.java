538c78a16080d9d68acb21a0e7968d1aa16383e3
Waldevv <133806522+Waldevv@users.noreply.github.com>
2024-07-29 04:18:42 -0400

    Update UsuarioRepository.java

--- backend/sigli/src/main/java/br/com/sigli/sigli/repository/UsuarioRepository.java
+++ backend/sigli/src/main/java/br/com/sigli/sigli/repository/UsuarioRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     List<Usuario> findByProjetoId(Long projetoId);
-    Optional<Usuario> findByEmail(String email); // Método para buscar usuário pelo email
+    Optional<Usuario> findByEmail(String email);
 }
